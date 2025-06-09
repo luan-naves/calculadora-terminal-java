1b60865660694b23b343805d44ebd7008a9b339d
luan-naves <luannf-@hotmail.com>
2025-06-09 16:45:44 -0300

    Melhora mensagens de erro e instruções para o usuário no Soma.java

--- Soma.java
+++ Soma.java
@@ -1,15 +1,20 @@
 public class Soma {
     public static void main(String[] args) {
         if (args.length != 2) {
-            System.out.println("Uso correto: java Soma <num1> <num2>");
+            System.out.println("Por favor, digite dois números inteiros separados por espaço");
+            System.out.println("Exemplo de uso correto: java Soma 20 20");
             return;
         }
-
+    try {
         int num1 = Integer.parseInt(args[0]);
         int num2 = Integer.parseInt(args[1]);
 
         int resultado = num1 + num2;
         System.out.println("A soma é: " + resultado);
+    } catch (NumberFormatException ex) {
+        System.out.println("Erro: Certifique-se de que ambos os argumentos são números inteiros válidos.");
+    }
+
     }
 }
 
